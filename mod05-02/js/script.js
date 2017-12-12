b0c1faf58be3b25046410f290a721f5c059427ab
Michael James Albatana <mjtalbatana@gmail.com>
2017-12-12 21:35:54 +0800

    mod05-02 activity

---
+++ mod05-02/js/script.js
@@ -0,0 +1,24 @@
+function redFunc(){
+  document.getElementById("red-box").style.background = "red";
+}
+function orangeFunc(){
+  document.getElementById("orange-box").style.background = "orange";
+}
+function yellowFunc(){
+  document.getElementById("yellow-box").style.background = "yellow";
+}
+function greenFunc(){
+  document.getElementById("green-box").style.background = "green";
+}
+function blueFunc(){
+  document.getElementById("blue-box").style.background = "blue";
+}
+function indigoFunc(){
+  document.getElementById("indigo-box").style.background = "indigo";
+}
+function violetFunc(){
+  document.getElementById("violet-box").style.background = "violet";
+}
+function whiteFunc(){
+  document.getElementsByClassName("box") = "white";
+}
