2eadf77cd31ae1850f35620789545ce23be30042
Michael James Albatana <mjtalbatana@gmail.com>
2017-12-23 22:54:08 +0800

    done self-programmed mod07-05, for publish to github

---
+++ mod07-05/js/script_old.js
@@ -0,0 +1,59 @@
+// function generateTable() {
+//   var num = document.inputform.num.value;
+//   var step = document.inputform.step.value;
+
+//   var row = num / step;
+//   var element = "";
+//   var boxClass = "";
+//   var classBox = "";
+
+//   var boxHeight = row / 4;
+
+//   boxHeight = Math.round(boxHeight);
+//   boxHeight = 100 / boxHeight;
+
+//   var z = step;
+//   for (var x = 0; x <= (row / 4); x++) {
+//     for (var y = 0; y < 4; y++) {
+//       if (step <= num) {
+//         if ((x % 2 == 0) && (y % 2 == 0)) {
+//           boxClass = "black";
+//         }
+//         if ((x % 2 == 0) && (y % 2 == 1)) {
+//           boxClass = "white";
+//         }
+//         if ((x % 2 == 1) && (y % 2 == 0)) {
+//           boxClass = "white";
+//         }
+//         if ((x % 2 == 1) && (y % 2 == 1)) {
+//           boxClass = "black";
+//         }
+//         element += "<div class ='box " + boxClass + "'><h2>" + z + "</h2></div>";
+//         z += step;
+
+//       }
+//     }
+//   }
+
+//   document.getElementsById("chessboard").innerHTML = element;
+//   classBox = document.getElementsByClassName("box");
+
+//   for (var i = 0; i < classBox.length; i++) {
+//     classBox[i].style.height = boxHeight + "vh";
+//     classBox[i].style.lineHeight = boxHeight + "vh";
+//   }
+
+// }
+
+function generateTable() {
+  var n = document.getElementById("n").value;
+  // for (i=0; i < n; i++) {
+  //   document.getElementById("tablegenerator").append
+  //   console.log(i);
+  // }
+  var node = document.createElement("LI");
+  var textnode = document.createElement("<h1>header</h1>");
+  node.appendChild(textnode);
+  document.getElementById("tablegenerator").appendChild(node);
+
+}
