1a8c35aacc649df20449da10725fcb04c4cfa8df
Michael James Albatana <mjtalbatana@gmail.com>
2017-12-24 22:33:22 +0800

    corrected "backspace" & continuous/sequential operation

--- mod07-06/js/script.js
+++ mod07-06/js/script.js
@@ -1,4 +1,5 @@
 var expression = "";
+var operator = "";
 function numEntry(value) {
   expression += value;
   document.getElementById('numScreen').innerHTML = expression;
@@ -9,6 +10,13 @@ function numClear() {
   expression = "";
 }
 
+function numOperator(operator){
+  expression = document.getElementById('numScreen').innerHTML;
+  expression += operator;
+  document.getElementById('numScreen').innerHTML = expression;
+}
+
+
 function numEquals() {
   expression = document.getElementById('numScreen').innerHTML;
   expression = eval(expression);
@@ -17,6 +25,7 @@ function numEquals() {
 }
 
 function numBack() {
+  expression = document.getElementById('numScreen').innerHTML;
   if (document.getElementById('numScreen').innerHTML == '') {
     numClear();
   }
