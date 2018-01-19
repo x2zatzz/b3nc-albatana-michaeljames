308e7721b1c16f68e7b907c08df2750c8299f3b2
Michael James Albatana <mjtalbatana@gmail.com>
2018-01-19 21:26:02 +0800

    roadblocks encountered, for continue activity after learning stuff

--- csp2/signup.php
+++ csp2/signup.php
@@ -1,36 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-  <head>
-  <?php include 'assets/php/head.php'?>
-  <title><?php retrieveTitle()?> | Welcome to Beeer Web App</title>
-  </head>
-<body>
-  <header>
-    <?php include 'assets/php/header.php'?>
-  </header>
-  <main>
-    <h1>Signup</h1>
-    <form id="signup" action="assets/php/authenticate.php" method="post">
-      <label for="username">username</label>
-      <input type="text" name="username">
-      <label for="firstname">firstname</label>
-      <input type="text" name="firstname">
-      <label for="lastname">lastname</label>
-      <input type="text" name="lastname">
-      <label for="password">password</label>
-      <input type="password" name="username">
-      <label for="confirmpassword">confirm password</label>
-      <input type="password" name="confirmpassword">
+$username = $_POST['username'];
+$password = $_POST['password'];
+// $passwordConfirm = htmlspecialchars($_POST['passwordConfirm']);
 
-      <input type="submit" value="Sign Up">
-    </form>
-  </main>
+echo $username . ' ' . $password . ' ' . $passwordConfirm;
 
-  <footer>
-    <?php include 'assets/php/footer.php'?>
-  </footer>
-<?php require 'assets/php/scripts.php'?>
-</body>
+// require 'assets/php/users.php';
+// array_push($users,[])
 
-</html>
+$file = file_get_contents('assets/json/users.json');
+$users = json_decode($file, true);
+
+var_export($users)
+
+
+?>
