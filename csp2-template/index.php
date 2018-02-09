2c31adb23a5c84f906011b8c401aa55e975a96fa
Michael James Albatana <mjtalbatana@gmail.com>
2018-02-09 21:24:23 +0800

    ACTIVITY: for continue, temporarily used the main activity files

--- csp2-template/index.php
+++ csp2-template/index.php
@@ -13,6 +13,7 @@
     <?php fn_layout_nav()?>
     <main>
       <h1>Index Page</h1>
+      <?php require_once('assets/php/dbconnect.php')?>
     </main>
     <footer class="panel panel-footer main-footer fixed-bottom">
       <?php fn_layout_footer()?>
