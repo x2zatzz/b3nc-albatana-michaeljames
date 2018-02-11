9f25a5e7adff193f6115b677c65e380fb200454f
Michael James Albatana <mjtalbatana@gmail.com>
2018-02-11 17:31:58 +0800

    CODE: PHP/HTML CODE ALPHA initial development
    LINE2: initialization file: init.php
    LINE4: disabled auto-title feature due to changes in DOM style
    LINE21,31,33,37: minor HTML attribute changes
    LINE49~58: create the PHP function template for 'snackbar'
    LINE59: disabled greensock.js library temporarily

--- csp2-template/assets/layouts.php
+++ csp2-template/assets/layouts.php
@@ -1,7 +1,8 @@
 <?php
-
+require_once 'assets/php/init.php';
 function fn_layout_head(){
-  $_SESSION['webtitle'] = str_replace('.php','',basename($_SERVER['PHP_SELF']));
+  // $_SESSION['webtitle'] = str_replace('.php','',basename($_SERVER['PHP_SELF']));
+
   echo "
     <meta charset=\"utf-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no\">
@@ -18,7 +19,7 @@ function fn_layout_head(){
 
 function fn_layout_nav(){
 echo "
-  <nav class=\"navbar navbar-default navbar-inverse\" id=\"nav-main\">
+  <nav class=\"navbar navbar-default navbar-inverse\" id=\"nav\">
     <div class=\"container-fluid\">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class=\"navbar-header\">
@@ -28,23 +29,31 @@ echo "
           <span class=\"icon-bar\"></span>
           <span class=\"icon-bar\"></span>
         </button>
-        <a class=\"navbar-brand\" href=\"#\">Kraff Beeer</a>
+        <a class=\"navbar-brand\" onclick=\"window.location.href='index.php'\">Kraff Beeer</a>
       </div>
-
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
         <ul class=\"nav navbar-nav navbar-right\">
-          <li><a href=\"#\">My Cart</a></li>
-          <li><a href=\"#\">About</a></li>
-          <li><a href=\"#\">Careers</a></li>
-          <li><a href=\"#\">Catalog</a></li>
-          <li><a href=\"#\">Sign In</a></li>
-          <li><a href=\"#\">Sign Up</a></li>
+          <li><a class=\"navlink\">My Cart</a></li>
+          <li><a class=\"navlink\">About</a></li>
+          <li><a class=\"navlink\">Careers</a></li>
+          <li><a class=\"navlink\">Catalog</a></li>
+          <li><a class=\"navlink\">Sign In</a></li>
+          <li><a class=\"navlink\">Sign Up</a></li>g
         </ul>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
 ";
+}
+
+function fn_layout_snackbar(){
+  $var = 'snackbar';
+  if(isset($_SESSION['snackbar'])){
+    $var = $_SESSION['snackbar'];
+  }
+  echo "
+    <div id=\"snackbar\" hidden>" . $var . "</div>";
 }
 
 
@@ -56,8 +65,9 @@ function fn_layout_footer(){
 }
 
 function fn_layout_libraries(){
-  echo "<script language=\"javascript\" src=\"../../dev-modules/greensock-js/greensock-js-1.20.3/src/minified/TweenMax.min.js\"></script>";
+  // echo "<script language=\"javascript\" src=\"../../dev-modules/greensock-js/greensock-js-1.20.3/src/minified/TweenMax.min.js\"></script>";
 }
+
 
 function fn_layout_signature(){
   echo "
@@ -74,4 +84,62 @@ function fn_layout_signature(){
   <!-- END OF SIGNATURE ===============================================+-->
   ";
 }
+
+//====================================================================
+function fn_page_index(){
+  $_SESSION['webtitle'] = 'Index';
+  echo "
+    <h1>Welcome Guest!</h1>
+  ";
+}
+
+
+function fn_page_signup(){
+  $_SESSION['webtitle'] = 'Register';
+  echo "
+  <h2>User Registration</h2>
+  <form action=\"assets/register.php\" method=\"POST\">
+    <fieldset name=\"username\">
+      <label for=\"username\">Username: </label>
+      <input id=\"username\" class=\"validate\" type=\"text\" name=\"username\"
+        value=\"value\" placeholder=\"enter a unique username\"
+        required autofocus autocomplete>
+    </fieldset>
+    <fieldset>
+      <label for=\"password\">Password: </label>
+      <input id=\"password\" type=\"password\" name=\"password\"
+        value=\"\" placeholder=\"enter your password\"
+        required autocomplete>
+    </fieldset>
+    <fieldset>
+      <label for=\"confirmpassword\">Confirm Password: </label>
+      <input id=\"confirmpassword\" class=\"validate\" type=\"password\" name=\"confirmpassword\"
+        value=\"\" placeholder=\"re-type your password\"
+        required autocomplete>
+    </fieldset>
+    <fieldset>
+      <label for=\"email\">Email Address: </label>
+      <input id=\"email\" class=\"validate\" type=\"email\" name=\"email\"
+        value=\"value@value.value\" placeholder=\"enter a valid email@domain\"
+        required autocomplete>
+    </fieldset>
+    <fieldset>
+      <label for=\"firstname\">First Name: </label>
+      <input id=\"firstname\" class=\"validate\" type=\"text\" name=\"firstname\"
+        value=\"value\" placeholder=\"enter your first name\"
+        required autocomplete>
+    </fieldset>
+    <fieldset>
+      <label for=\"lastname\">Last Name: </label>
+      <input id=\"lastname\" class=\"validate\" type=\"text\" name=\"lastname\"
+        value=\"value\" placeholder=\"enter your last name\"
+        required autocomplete>
+    </fieldset>
+    <fieldset>
+      <button id=\"submitbutton\" type=\"submit\">Register</button>
+    </fieldset>
+  </form>
+  ";
+}
+
 ?>
